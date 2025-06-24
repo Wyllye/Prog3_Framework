6e0d161346e6a13507c40a1cd7aacd8481131a42
Vinícius A. Wille <viniciusandreiwille@gmail.com>
2025-06-24 13:49:21 -0300

    Remove a pasta Programacao/estoque e seu conteúdo do controle de versão

--- Programacao/estoque/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=127.0.0.1;port=3306;dbname=estoque_db',
-    'username' => 'root',
-    'password' => '',        // normalmente em XAMPP o root não tem senha
-    'charset'  => 'utf8mb4',
-];
